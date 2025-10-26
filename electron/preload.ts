226a5738ac28a0c537888014a8b7e076ea2921f5
adomashev14-rgb <adomashev14@gmail.com>
2025-10-27 00:26:01 +0200

    Update preload.ts

--- electron/preload.ts
+++ electron/preload.ts
@@ -1,13 +1,6 @@
+// electron/preload.ts
 import { contextBridge, ipcRenderer } from 'electron';
 
-type StoreAPI = {
-  get: (key: string) => Promise<unknown>;
-  set: (key: string, value: unknown) => Promise<void>;
-};
-
-const store: StoreAPI = {
-  get: (key: string) => ipcRenderer.invoke('store:get', key),
-  set: (key: string, value: unknown) => ipcRenderer.invoke('store:set', key, value),
-};
-
-contextBridge.exposeInMainWorld('electronStore', store);
+contextBridge.exposeInMainWorld('api', {
+  ping: () => ipcRenderer.invoke('ping'),
+});
